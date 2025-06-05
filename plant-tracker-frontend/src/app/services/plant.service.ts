a704ccb2a1ec6b9428384175c2b6c26cf34ac3c0
Juan Garcia <johnnyjgarcia1996@gmail.com>
2025-06-05 00:10:55 -0500

    added environment files for prod

--- plant-tracker-frontend/src/app/services/plant.service.ts
+++ plant-tracker-frontend/src/app/services/plant.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';  // <-- Import environment
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantService {
-  private apiUrl = 'https://localhost:7015/api/plants';
+  private apiUrl = `${environment.apiUrl}/plants`;  // <-- Use environment apiUrl
 
   constructor(private http: HttpClient) {}
 
